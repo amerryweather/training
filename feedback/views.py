34d283f292a67f161820f091442d09e260bc9003
Angie Merryweather <hi@angie.io>
2014-03-04 16:29:15 +0000

    Removed non-functional index.views section

--- feedback/views.py
+++ feedback/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from feedback.models import Attendee
 
 # Create your views here.
 
 def index(request):
 	context = RequestContext(request)
 	context_dict = {}
-	
-	attendee_list = Attendee.Objects.all()
-	context_dict['attendees'] = attendee_list
+		
 	return render_to_response('feedback/index.html', {}, context)
